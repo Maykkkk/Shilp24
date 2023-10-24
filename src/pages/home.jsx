cf84072d913b16040821c70e2b5851c251420636
AbhijeetMankani <abhijeetmankani@gmail.com>
2023-10-24 11:01:07 +0530

    Added File Struture All the pages go in the pages Folder in src All the CSS, Images, and other linkings go in the links folder in src
    Added Routing to the project

---
+++ src/pages/home.jsx
@@ -0,0 +1,17 @@
+// import logo from '../logo.svg';
+import '../links/css/home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header> */}
+      <body>
+        <h1>HOME</h1>
+      </body>
+    </div>
+  );
+}
+
+export default Home;
