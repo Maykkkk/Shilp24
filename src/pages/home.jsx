502b1d33ab1912d90d347cead977ef1532202a8c
Abhishek Shukla <abhishekshukla@192.168.1.2>
2023-12-16 21:39:19 +0530

    shilp logo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import '../links/css/home.css';
 import NavBar from '../components/NavBar';
 import ImageGallery from '../components/ImageGallery';
 import Footer from '../components/Footer';
-
+import Shilp from '../components/shilp';
 
 function Home() {
   return (
@@ -18,7 +18,7 @@ function Home() {
           <h2 className='text-light m-0 p-0'>Civil Engineering Society</h2>
           </div>
         </div>
-        <div className="container" id='competitions'></div>
+        <div className="container" id='competitions'><Shilp/></div>
         <div className="container" id='competitions'>
           <ImageGallery></ImageGallery>
         </div>
