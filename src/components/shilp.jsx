b5153dcddaada126386be09149db7d46fe22b9f7
Abhishek Shukla <abhishekshukla@192.168.1.2>
2023-12-16 19:03:24 +0530

    added innovation oddeseey 2

--- src/components/shilp.jsx
+++ src/components/shilp.jsx
@@ -7,7 +7,7 @@ function Shilp() {
 
         <div class="contain">
             <div class="shilp-logo">
-                <img src={shilp_logo} />
+                <img src={shilp_logo}  alt=""/>
             </div>
             <div class="description">
                 Shilp, the annual fest by the Civil Engineering Society, is a dynamic celebration of creativity and innovation in the field. Serving as a pivotal platform, Shilp fosters collaboration, promotes innovation, and values inclusivity. Beyond a mere event, Shilp's significance lies in its commitment to inspiring a culture of learning, pushing boundaries, and preparing participants for the professional world. With a focus on both technical and soft skills, Shilp stands as a vibrant testament to the values that drive the civil engineering community forward, shaping the next generation of engineers to embrace challenges and contribute meaningfully to the field.
