2a97798f87ef3a7cf1077b1810622be9ef070932
AbhijeetMankani <abhijeetmankani@gmail.com>
2023-12-17 12:43:30 +0530

    Revert "1st 100vh + navbar"
    This reverts commit a6bdcf2e1183e8c09926127d81c723b7fc5e1d99.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// import logo from './logo.svg';
 import "./App.css";
 import Home from "./pages/home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,25 +1,15 @@
+// import logo from '../logo.svg';
 import '../links/css/home.css';
-import NavBar from '../components/NavBar';
 import Shilp from '../components/shilp';
 
 function Home() {
   return (
     <div className="App">
-      <NavBar></NavBar>
-      <body className="body">
-        <div className="container d-flex align-items-center justify-content-center">
-          <div id="title" className=''>
-          <h1 className='m-0 p-0 '>
-            <span className="primary x">SHILP </span> 
-            <span className="text-light">IIT BHU</span>
-            </h1>
-          <h2 className='text-light m-0 p-0'>Civil Engineering Society</h2>
-          </div>
-        </div>
-        <div className="container"></div>
-        <div>
-          <Shilp />
-        </div>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header> */}
+      <body>
+        <h1>HOME</h1>
       </body>
     </div>
   );
