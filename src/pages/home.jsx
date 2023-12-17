1cb327abd6511ac35a283cb0cbd806996815362f
AbhijeetMankani <abhijeetmankani@gmail.com>
2023-12-17 12:50:55 +0530

    Revert before unreviewed push

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,21 @@
 // import logo from '../logo.svg';
 import '../links/css/home.css';
-import Shilp from '../components/shilp';
 
 function Home() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header> */}
-      <body>
-        <h1>HOME</h1>
+      <NavBar></NavBar>
+      <body className="body">
+        <div className="container d-flex align-items-center justify-content-center">
+          <div id="title" className=''>
+          <h1 className='m-0 p-0 '>
+            <span className="primary x">SHILP </span> 
+            <span className="text-light">IIT BHU</span>
+            </h1>
+          <h2 className='text-light m-0 p-0'>Civil Engineering Society</h2>
+          </div>
+        </div>
+        <div className="container"></div>
       </body>
     </div>
   );
