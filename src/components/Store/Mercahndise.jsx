60a9c4e81d717f5008f281bac237ebe8d500513a
AbhijeetMankani <abhijeetmankani@gmail.com>
2023-12-23 03:14:01 +0530

    Styled store page buy buttons

--- src/components/Store/Mercahndise.jsx
+++ src/components/Store/Mercahndise.jsx
@@ -7,27 +7,28 @@ function Merch() {
  return (
     <div className="merch-container">
       <h1 className="top">SHILP MERCHANDISE</h1>
+
       <div className="shop-item">
-      
         <h2>T-shirt 1</h2>
-        <img className="merch-img" src={tshirt}/>
+        <img className="merch-img" src={tshirt} alt=""/>
         <p className='price'>INR 700 900</p>
-        <button>Buy Now</button>
+        <button className='buy-button'>Buy Now</button>
       </div>
+
       <div className="shop-item">
-      
         <h2>T-shirt 2</h2>
-        <img className="merch-img" src={tshirt}/>
+        <img className="merch-img" src={tshirt} alt=""/>
         <p className='price'>INR 700 900</p>
-        <button>Buy Now</button>
+        <button className='buy-button'>Buy Now</button>
       </div>
+
       <div className="shop-item">
-      
         <h2>Hoodie</h2>
-        <img className="merch-img" src={hoodie}/>
+        <img className="merch-img" src={hoodie} alt=""/>
         <p className='price'>INR 1000 1200</p>
-        <button>Buy Now</button>
+        <button className='buy-button'>Buy Now</button>
       </div>
+
     </div>
  );
 };
