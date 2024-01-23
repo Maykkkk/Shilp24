6b24091f529eb1a5707b42834dcf22cb2c5523a4
Abhishek Shukla <abhishekshukla@Abhisheks-MacBook-Air-2.local>
2024-01-23 21:52:38 +0530

    login page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -42,6 +42,12 @@ function NavBar() {
                     <li className="nav-item px-2 mx-2">
                     <Link className="nav-link" to='https://www.youtube.com/watch?v=xvFZjo5PgG0'>Media</Link>
                     </li>
+                    <li className="nav-item px-2 mx-2">
+                        <Link className="nav-link" to='/login'>Login</Link>
+                    </li>
+                    <li className="nav-item px-2 mx-2">
+                        <Link className="nav-link" to='/register'>Register</Link>
+                    </li>
                 </ul>
             </div>
         </nav>
