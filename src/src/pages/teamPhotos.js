c3b327e5eba5344b8b3613e1bcc1741783572565
wiz-abhishek <131913148+wiz-abhishek@users.noreply.github.com>
2024-01-26 10:39:48 +0530

    navbar and all

---
+++ src/src/pages/teamPhotos.js
@@ -0,0 +1,37 @@
+const teamPhotos ={
+    // Advisors
+    president: "./TeamPhotos/IV Year/AYUSH SINHA_PRESIDENT(STUDENTS), CES.jpeg_page-0001.jpg",
+    conveynor:"./TeamPhotos/IV Year/Ashutosh Gupta_ Convenor.jpg",
+    coConveynor1:"./TeamPhotos/IV Year/Kanhaiya Kumar _ Co-Convener.jpg",
+    coConveynor2 : "/TeamPhotos/III Year/NANDINI BHIMSARIA_ CO-CONVENOR_page-0001 (1).jpg",
+    techAdvisor:"./TeamPhotos/IV Year/JEEVESH GARG _ TECH ADVISOR.jpg",
+    eventsAdvisor1:"./TeamPhotos/IV Year/ANUP TIWARI II EVENTS ADVISOR.jpg",
+    eventsAdvisor2:"./TeamPhotos/IV Year/NAVVAY DHINGRA _ EVENTS.jpg",
+    eventsAdvisor3:"./TeamPhotos/IV Year/Pawan Kumar events advisor part 4.jpg",
+    publicityAdvisor:"./TeamPhotos/IV Year/Assim Ujjwal _ Publicity Advisor.jpg",
+
+    // Heads
+    eventsHead1 : "./TeamPhotos/III Year/DEVESH SINDHI _ EVENTS HEAD/IMG-20231015-WA0003.jpg",
+    eventsHead2 : "./TeamPhotos/III Year/PARAM SRIVASTAVA _ EVENTS HEAD.jpg",
+    eventsHead3 : "./TeamPhotos/III Year/PRACHI PRIYA _ EVENTS HEAD.jpg",
+    eventsHead4 : "./TeamPhotos/III Year/Pratik Nand _ Events Head.jpg",
+    eventsHead5 : "./TeamPhotos/III Year/Radhika Singh Rajawat _ Events Head.jpg",
+    publicityHead1 : "./TeamPhotos/III Year/AKHAND PRATAP YADAV _ PUBLICITY HEAD.jpg",
+    publicityHead2 : "./TeamPhotos/III Year/ANCHAL GANGULY _ PUBLICITY HEAD.jpg",
+    publicityHead3 : "./TeamPhotos/III Year/MANVI SRIVASTAVA _ PUBLICITY HEAD.jpg",
+    marketingHead1 : "./TeamPhotos/III Year/Aman Srivastava _ MARKETING HEAD.png",
+    marketingHead2 : "./TeamPhotos/III Year/MITHILESH K _ MARKETING HEAD.jpg",
+    marketingHead3 : "./TeamPhotos/III Year/PRAKHAR YADAV _ MARKETING HEAD.jpg",
+    marketingHead4 : "./TeamPhotos/III Year/PRIYARANJAN KUMAR KHAN _ MARKETING HEAD.jpeg",
+    designContentHead1 : "./TeamPhotos/III Year/Ankit Kumar _ Content Head.jpg",
+    designContentHead2 : "./TeamPhotos/III Year/ASHWANI KUMAR SHARMA ll DESIGN HEAD.jpeg",
+    designContentHead3:"./TeamPhotos/III Year/MANSI PANDHARPURE _ DESIGN HEAD.jpg",
+    prHead1:"./TeamPhotos/III Year/AYUSH SAHU _ PR HEAD.jpg",
+    prHead2:"./TeamPhotos/III Year/NISHKARSH SHARMA PR HEAD.jpg",
+    prHead3 : "./TeamPhotos/III Year/PRATHAM CHAUDHARY _ PR HEAD .jpg",
+    prHead4 : "./TeamPhotos/III Year/SUMANA SREE II PR HEAD.jpg",
+    techHead : "./TeamPhotos/III Year/MAYANK MANI NATH GUPTA _ TECH HEAD.png",
+
+
+};
+export default teamPhotos;
