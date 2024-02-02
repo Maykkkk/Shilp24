de7c0e38a1fb9188968ddc8787901c6cba444012
Amit Dutta <amitdutta2406@gmail.com>
2024-02-02 23:57:18 +0530

    Added loaders to all page

--- client/src/pages/competitions.jsx
+++ client/src/pages/competitions.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import "../links/css/Competition.css";
 import EmptyNote from "../components/EmptyNote";
 import Fade from "react-reveal/Fade";
+import ParticleBackground from "../components/ParticleBackground";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
-const competitions = ({ AllAuth }) => {
+const override = {
+	display: "block",
+	margin: "0 auto",
+	borderColor: "red",
+  };
+
+
+const Competitions = ({ AllAuth }) => {
+
+  const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+	}, []);
+
 	return (
 		<div className="App">
+      {loading ? (
+				<div className="loader-container" style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh", background:"#271e29"}}>
+					<ParticleBackground />
+					<PacmanLoader
+						color="#36d7b7"
+						loading={loading}
+						cssOverride={override}
+						size={50}
+						aria-label="Loading Spinner"
+						data-testid="loader"
+					/>
+				</div>
+			) : 
 			<div className="body">
 				<NavBar AllAuth={AllAuth} />
 				{/* <div className='cards'>
@@ -53,8 +84,9 @@ const competitions = ({ AllAuth }) => {
 					<Footer></Footer>
 				</Fade>
 			</div>
+}
 		</div>
 	);
 };
 
-export default competitions;
+export default Competitions;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react";
 import "../links/css/home.css";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
@@ -7,26 +8,56 @@ import Shilp from "../components/shilp";
 import About from "../components/About";
 import ExploreMore from "../components/ExploreMore";
 import ParticleBackground from "../components/ParticleBackground";
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+const override = {
+	display: "block",
+	margin: "0 auto",
+	borderColor: "red",
+  };
 
 const Home = ({ AllAuth }) => {
+
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+	}, []);
+
+
 	return (
 		<div className="App">
-			<div className="home-body">
-				<ParticleBackground />
-				<NavBar AllAuth={AllAuth} />
-				<Hero />
-				<Shilp />
-				<div className="container" id="gallery">
-					<ImageGallery></ImageGallery>
+			{loading ? (
+				<div className="loader-container" style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh", background:"#271e29"}}>
+					<ParticleBackground />
+					<PacmanLoader
+						color="#36d7b7"
+						loading={loading}
+						cssOverride={override}
+						size={50}
+						aria-label="Loading Spinner"
+						data-testid="loader"
+					/>
 				</div>
-				<div className="container-about-events-footer">
-					<About />
-					<ExploreMore />
-				</div>
-				<div className="Bottom">
-					<Footer />
-				</div>
+			) : <div className="home-body">
+			<ParticleBackground />
+			<NavBar AllAuth={AllAuth} />
+			<Hero />
+			<Shilp />
+			<div className="container" id="gallery">
+				<ImageGallery></ImageGallery>
 			</div>
+			<div className="container-about-events-footer">
+				<About />
+				<ExploreMore />
+			</div>
+			<div className="Bottom">
+				<Footer />
+			</div>
+		</div>
+		}
 		</div>
 	);
 };
