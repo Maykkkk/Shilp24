4492dfb1ac685c2a469112a7970fc10691472bdd
Amit Dutta <amitdutta2406@gmail.com>
2024-03-21 19:06:00 +0530

    Workshop Details Added

--- client/src/components/WorkshopCard.jsx
+++ client/src/components/WorkshopCard.jsx
@@ -4,7 +4,57 @@ import "../links/css/pdf.css";
 
 
 function WorkshopCard(props) {
+
+	const [staad, setStaad] = React.useState(false);
+	const [bim, setBim] = React.useState(false);
+
+	const showStaad = () => {
+		setStaad(true);
+	}
+	const closeStad = () => {
+		setStaad(false);
+	}
+	const showBim = () => {
+		setBim(true);
+	}
+	const closeBim = () => {
+		setBim(false);
+	}
+
     return (
+		<>{staad &&
+		<div className="detailCardContainer">
+			<div className="detailCard">
+				<button className="closeButton" onClick={closeStad}><b>X</b></button>
+				<h1>Details</h1>
+				<ul className="detailCardList">
+					<li className="detailCardListItem">An Introductory workshop on Staad PRO with  the practice excersises in between</li>
+					<li className="detailCardListItem"><b>Mr. Kaviyasaran D.</b>, chief engineer and founder of Avi engineering services, who is also an alum of IIT Madras will guide us</li>
+					<li className="detailCardListItem">It would be a 3-4 hour workshop , Mr Kaviyasaran will teach us from the basics of Staad pro and end it with a project</li>
+					<li className="detailCardListItem"><b>No prior knowledge on Staad Pro is needed</b></li>
+					<li className="detailCardListItem"><b>Prerequisites:</b> Attendees should bring their laptops with Staad pro (student version) pre-installed in it.</li>
+					<li className="detailCardListItem"><b>The price :</b> free with shilp registration fee. Participants have to pay shilp registration fee only</li>
+				</ul>
+			</div>
+		</div>
+		}
+		{bim &&
+		<div className="detailCardContainer">
+			<div className="detailCard">
+				<button className="closeButton" onClick={closeBim}><b>X</b></button>
+				<h1>Details</h1>
+				<h6 style={{marginTop:"25vh"}}>Discover BIM in just 1-2 hours. Get a certificate upon completion and Elevate your skills with us!</h6>
+				{/* <ul className="detailCardList">
+					<li className="detailCardListItem">An Introductory workshop on Staad PRO with  the practice excersises in between</li>
+					<li className="detailCardListItem">Kaviyasaran_d, chief engineer and founder of Avi engineering services, who is also an alum of IIT Madras will guide us</li>
+					<li className="detailCardListItem">It would be a 3-4 hour workshop , he will teach us from the basics of Staad pro and end it with a project</li>
+					<li className="detailCardListItem"><b>No prior knowledge on Staad Pro is needed</b></li>
+					<li className="detailCardListItem"><b>Prerequisites:</b> Attendees should bring their laptops with Staad pro (student version) pre-installed in it.</li>
+					<li className="detailCardListItem"><b>The price :</b> free with shilp registration fee. Participants have to pay shilp registration fee only</li>
+				</ul> */}
+			</div>
+		</div>
+		}
 		<div
 			className="card"
 			style={{
@@ -20,8 +70,15 @@ function WorkshopCard(props) {
 				>
 					Register
 				</button>
+				<button
+					className="button"
+					onClick={props.name === "STAAD Pro" ? showStaad : showBim}
+				>
+					Details
+				</button>
 			</div>
 		</div>
+		</>
 	);
 }
 
--- client/src/pages/competitions.jsx
+++ client/src/pages/competitions.jsx
@@ -2,10 +2,10 @@ import React,{useEffect,useState} from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import "../links/css/workshop.css";
-import EmptyNote from "../components/EmptyNote";
 import Fade from "react-reveal/Fade";
 import ParticleBackground from "../components/ParticleBackground";
 import PacmanLoader from "react-spinners/PacmanLoader";
+import WorkshopCard from "../components/WorkshopCard";
 
 const override = {
 	display: "flex",
@@ -13,18 +13,13 @@ const override = {
 	alignItems: "center",
 	borderColor: "red",
   };
-
-
 const Competitions = ({ AllAuth }) => {
-
   const [loading, setLoading] = useState(true);
-
 	useEffect(() => {
 		setTimeout(() => {
 			setLoading(false);
 		}, 1000);
 	}, []);
-
 	return (
 		<div className="App">
       {loading ? (
@@ -40,54 +35,33 @@ const Competitions = ({ AllAuth }) => {
 					/>
 				</div>
 			) : 
-			<div className="body">
-				<NavBar AllAuth={AllAuth} />
-				{/* <div className='cards'>
-    <EventCard
-      name="civil Explo"
-      details="civil ka kuch kaam hota hai"
-    />
-    <EventCard
-      name="cp related"
-      details="rating badhao"
-    />
-    <EventCard
-      name="ml related"
-      details="big data machine learning modi ji"
-    />
-    <EventCard
-      name="civil Explo"
-      details="civil ka kuch kaam hota hai"
-    />
-    <EventCard
-      name="cp related"
-      details="rating badhao"
-    />
-    <EventCard
-      name="ml related"
-      details="big data machine learning modi ji"
-    />
-    <EventCard
-      name="civil Explo"
-      details="civil ka kuch kaam hota hai"
-    />
-    <EventCard
-      name="cp related"
-      details="rating badhao"
-    />
-    <EventCard
-      name="ml related"
-      details="big data machine learning modi ji"
-    />
-    </div> */}
-    <EmptyNote/>
-				<Fade bottom>
-					<Footer></Footer>
-				</Fade>
-			</div>
-}
+      <>
+			<div className="body workshop-body">
+        <ParticleBackground />
+				<NavBar AllAuth={AllAuth}/>
+        <div className="workshopHeading">
+          <div className="workshopTitle">
+            Workshops
+          </div>
+        </div>
+        <div className='cards'>
+          <WorkshopCard
+            name="STAAD Pro"
+            details="Explore STAAD Pro in our 3-4 hour workshop. Gain hands-on experience and receive a course certificate!"
+          />
+          <WorkshopCard
+            name="BIM"
+            details="Discover BIM in just 1-2 hours. Get a certificate upon completion and Elevate your skills with us!"
+          />
+        </div>
+      <Fade bottom>
+          <Footer></Footer>
+        </Fade>
+        </div>
+    </>
+    }
 		</div>
 	);
-};
+}
 
 export default Competitions;
